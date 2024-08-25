8cedeeb631a140cdf94a06849e93e0bef19d0e1b
Adetunji Adeniran <AAdeniran001@dundee.ac.uk>
2024-08-25 17:40:27 +0100

    update 25/08/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {Layout, Breadcrumb, theme , Card, Carousel} from 'antd'
+import {Layout, Breadcrumb, theme , Card, Carousel, Divider} from 'antd'
 import './App.css'
 const{Content, Footer} = Layout;
 
@@ -18,51 +18,96 @@ function App() {
 return (
   
   <Layout className='container'>
-  <header className="header">
+  
+    <header className="header">
       <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet"></link>
-  <div style={{ display: 'flex', alignItems: 'center' }}>
-    <img className='logo'
-      src="images/logo3.jpg"  // Replace with the path to your logo image
-      alt="Company Logo" 
-      />
-    <Breadcrumb className='menu'>
-      <Breadcrumb.Item><a href="/home">Home</a></Breadcrumb.Item>
-      <Breadcrumb.Item><a href="/our-people">Our people</a></Breadcrumb.Item>
-      <Breadcrumb.Item><a href="/our-projects">Our projects</a></Breadcrumb.Item>
-    </Breadcrumb>
-  </div>
-</header>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img className='logo'
+            src="images/logo3.jpg"  // Replace with the path to your logo image
+            alt="Company Logo" />
+          <Breadcrumb className='menu'>
+            <Breadcrumb.Item><a href="#section1">Home</a></Breadcrumb.Item>
+            <Breadcrumb.Item><a href="#section2">Team</a></Breadcrumb.Item>
+            <Breadcrumb.Item><a href="#section3">Our projects</a></Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
+      </header>
    
-   <Content className='content-container'>
+    <Content className='content-container'>
    
-    <div>
-      <Card  className='home-card'
-        title="Our Mission" 
-        hoverable
-       >
-         <Carousel autoplay autoplaySpeed={30000} speed={1000}>
-       <p className='mission-paragraph'>
-        Leading the Revolution with Cutting-Edge Technology and Innovative Solutions for a Better Tomorrow.
-        </p>
-        <p className='mission-paragraph'>
-        Empowering the next generation of tech leaders and bridging the digital divide by offering 
-        widespread access to top-tier technology training for all.
-        </p>
-        </Carousel>
-      </Card>
-    </div>
-    <div>
-      This is another div
-    </div>
+      <div id='section1' className='home-card-container'>
+        <Card  className='home-card'
+              title="Our Mission" 
+              hoverable>
+          <Carousel autoplay autoplaySpeed={30000} speed={1000}>
+            <p className='mission-paragraph'>
+                Leading the Revolution with Cutting-Edge Technology and Innovative Solutions for a Better Tomorrow.
+            </p>
+            <p className='mission-paragraph'>
+                Empowering the next generation of tech leaders and bridging the digital divide by offering 
+                widespread access to top-tier technology training for all.
+            </p>
+          </Carousel>
+        </Card>
+      </div>
 
-    
-  </Content>
-  <Footer
+  <Divider className="custom-divider">
+    <b>Our Team</b>
+  </Divider>
+    <div id='section2' className='profile-div-container'>
+      <table className='team-members'>
+        <tr>
+          <td>
+            <Card  hoverable>
+              <div className='profile-div'>
+                <img className='profile-img' src="/images/MD_CEO.jpg" alt='Adetunji Adeniran' />
+                  <p className='profile-text'>
+                    <a 
+                      href="/Tunji-profile-page" 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                      >
+                      Adetunji Adeniran
+                    </a>
+                  <br />
+                    Team/Tech Lead.
+                  </p>
+              </div>
+            </Card>
+          </td>
+          <td>
+          <Card  hoverable>
+              <div className='profile-div'>
+                <img className='profile-img' src="/images/diran.jpg" alt='Diran Adeniran' />
+                  <p className='profile-text'>
+                    <a 
+                      href="/diran-profile-page" 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                      >
+                      Diran Adeniran
+                    </a>
+                  <br />
+                    Operation Lead.
+                  </p>
+              </div>
+            </Card>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <Divider className="custom-divider">
+    <b>Our Projects</b>
+  </Divider>
+    <div id='section3' className='project-div-container'>
+      Project container
+    </div>
+    </Content>
+      <Footer
         style={{
           textAlign: 'center',
-        }}
-      >
-        VERSAPRO©{new Date().getFullYear()} Created by VERSAPRO TECH
+        }}>
+        VERSAPRO TECH©{new Date().getFullYear()} 
       </Footer>
   </Layout>
  );

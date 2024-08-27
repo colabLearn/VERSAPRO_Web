8aac4b9e9fcedd89d2cc9c5bccb47e5cc9e9d9d8
Adetunji Adeniran <AAdeniran001@dundee.ac.uk>
2024-08-27 23:23:24 +0100

    Deploy Trials

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //base: '/VERSAPRO_Web/',
   plugins: [react(), eslint()],
   server: {
     port: 3000,

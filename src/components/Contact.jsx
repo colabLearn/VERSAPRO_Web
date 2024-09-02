31a3d89e194d9f8960fbc543885bbb5335cfefce
Adetunji Adeniran <AAdeniran001@dundee.ac.uk>
2024-09-03 00:07:43 +0100

    Update Address to include German Address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import { MailOutlined, PhoneOutlined, HomeOutlined } from '@ant-design/icons';
+import { HomeOutlined, PhoneOutlined, MailOutlined } from '@ant-design/icons';
+import '../App.css'; 
 
 const Contact = () => {
   return (
     <div className="contact-container">
       <center><h2>Contact Us</h2></center>
-      <address>
-        <p>
-          <HomeOutlined /> VERSAPRO TECH<br />
-          1 Tranent Walk<br />
-          Dundee, United Kingdom <br />
-         DD40XW <br />
-        </p>
-        <p>
-          <PhoneOutlined /> +44(0)7770093639
-        </p>
-        <p>
-          <MailOutlined /> <a href="mailto:colablearn2024@gmail.com">colablearn2024@gmail.com</a>
-        </p>
-      </address>
+      <div className="address-wrapper">
+        <address className="address-item">
+          <p>
+            <HomeOutlined /> VERSAPRO TECH<br />
+            1 Tranent Walk<br />
+            Dundee, DD40XW <br />
+             United Kingdom.<br />
+          </p>
+          <p>
+            <PhoneOutlined /> +44(0)7770093639
+          </p>
+          <p>
+            <MailOutlined /> <a href="mailto:colablearn2024@gmail.com">colablearn2024@gmail.com</a>
+          </p>
+        </address>
+        
+        <address className="address-item">
+          <p>
+            <HomeOutlined /> VERSAPRO TECH<br />
+            Simrock str 85<br />
+            40237, Dusseldorf <br />
+            Germany. <br />
+          </p>
+          <p>
+            <PhoneOutlined /> +49(0)17620569849
+          </p>
+          <p>
+            <MailOutlined /> <a href="mailto:adediranao@gmail.com">adediranao@gmail.com</a>
+          </p>
+        </address>
+      </div>
     </div>
   );
 };
